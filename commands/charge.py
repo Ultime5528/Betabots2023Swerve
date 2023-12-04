37cd1109d2ad068483da2eb064b76875027d6bc6
Javier <4101119@keranna.qc.ca>
2023-12-04 17:48:12 -0500

    Update charge.py

--- commands/charge.py
+++ commands/charge.py
@@ -9,16 +9,21 @@ class Charge(SafeCommand):
     distance3 = autoproperty(0.0)
     threshold = autoproperty(0.0)
 
-    def __init__(self, catapult: Catapult):
+    def __init__(self, catapult: Catapult, distanceWanted: int):
         super().__init__()
         self.catapult = catapult
         self.addRequirements(self.catapult)
+        self.get_distance = {
+            1: lambda: self.distance1,
+            2: lambda: self.distance2,
+            3: lambda: self.distance3
+        }[distanceWanted]
 
     def execute(self) -> None:
         self.catapult.moveUp()
 
     def isFinished(self) -> bool:
-        return abs(self.catapult.encoder.getPosition() - self.distance1) < self.threshold
+        return abs(self.catapult.encoder.getPosition() - self.get_distance()) < self.threshold
 
     def end(self, interrupted: bool) -> None:
         self.catapult.stopArm()
