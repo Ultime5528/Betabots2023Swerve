49356286955166e53c218b66550e583bca3595dc
Thomas McDonald <53022931+ThomasMcDonald904@users.noreply.github.com>
2023-12-11 07:54:38 -0500

    Fixed getting rotation

--- commands/vision/align.py
+++ commands/vision/align.py
@@ -12,10 +12,10 @@ class Align(SafeCommand):
         self.centered_tag_x = lambda: NetworkTables.getEntry("vision/tag_x")
         self.centered_tag_y = lambda: NetworkTables.getEntry("vision/tag_y")
         self.centered_tag_rot = lambda: NetworkTables.getEntry("vision/tag_rot")
-        self.estimator = self.drivetrain.getEstimator()
+
 
     def execute(self):
-        self.rot_error = self.centered_tag_rot - self.estimator.getEstimatedPosition().rot
+        self.rot_error = self.centered_tag_rot - self.drivetrain.getRotation()
         vx = math.copysign(self. centered_tag_x, centering_speed) * -1
         vr = math.copysign(self.rot_error, self.centering_rot_speed)
 
